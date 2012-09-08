540ba06633bad7a5af9d65851e8a98541426ee00
Mikko Waris <mikko.waris@gmail.com>
2012-09-08 09:19:40 +0300

    Fix handling of invalid sleeps

--- webapp/templatetags/dreamutils.py
+++ webapp/templatetags/dreamutils.py
@@ -6,7 +6,11 @@ register = template.Library()
 def hoursminutes(seconds):
     if seconds is None:
         return seconds
-
+    
+    if not isinstance(seconds, int):
+        return ""
+    
     m, s = divmod(seconds, 60)
     h, m = divmod(m, 60)
+    
     return "%d hours %d minutes" % (h, m)
